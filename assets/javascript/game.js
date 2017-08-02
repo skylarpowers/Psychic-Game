90aae0264c776ab5d1395892cd47da416ab0d72c
Skylar Powers <skylarpowers@Skylars-MacBook-Pro.local>
2017-08-02 10:19:57 -0700

    Redo

---
+++ assets/javascript/game.js
@@ -0,0 +1,9 @@
+//array for possible choices
+//var choices = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+//On click event
+//document.onkeyup = function(event) {
+	//var click = String.fromCharCode(event.keyCode).toLowerCase();
+//}
+
+//Selector for random letter
+//var random = Math.floor(Math.random() *26) + 1;
